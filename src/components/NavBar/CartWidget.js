e2a4eb0c22d7b4d4d2b8964f4e44928a2c59eb41
IsaacEST <117696107+IsaacEST@users.noreply.github.com>
2023-08-22 20:31:49 -0300

    Update CartWidget.js

--- src/components/NavBar/CartWidget.js
+++ src/components/NavBar/CartWidget.js
@@ -1,7 +1,21 @@
 import React, { useContext } from 'react';
-
+import { CartContext } from '../../context/CartContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
+const CartWidget = () => {
+    const { itemQuantity } = useContext(CartContext);
 
-
+    return (
+        <div className="header-icons">
+            <FontAwesomeIcon icon={faCartShopping} />
+            { itemQuantity === 0 ? (
+                <div className="qty-display"></div>
+            ) : (
+                <div className="qty-display">{itemQuantity()}</div>
+            )}
+        </div>
+    );
+  };
+  
+  export default CartWidget;
