c346caf3de80d7e2eafec9448cda2ad2a8fd1fe7
IsaacEST <117696107+IsaacEST@users.noreply.github.com>
2023-08-22 20:31:15 -0300

    Update ItemDetail.js

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,14 +1,31 @@
 import React, { useState, useContext } from 'react';
-
-
+import { CartContext } from '../../context/CartContext';
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import ItemCount from './ItemCount';
 import WhatsApp from '../../WhatsAppButtonGreenSmall.png';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../main.css';
 
 const ItemDetail = ({ item }) => {
-	
+	const [add, setAdd] = useState(false);
+	const { addItem } = useContext(CartContext)
 
+    const onAdd = (quantity) => {
+		setAdd(true);
+		addItem(item, quantity)
+		toast.success(`Agregaste ${quantity} ${item.title} (${item.detail})`, {
+			theme: "light",
+			position: "top-center",
+			autoClose: 1500,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		});
+    };
 
     return (
 		<>
